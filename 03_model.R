acc0cdff1457ac124decec7deab74079e03110bc
Tom Bergkamp <tombergkamp@planet.nl>
2021-10-18 12:38:40 +0200

    Add models and data with driver nested in year

--- 03_model.R
+++ 03_model.R
@@ -58,3 +58,29 @@ fit_weather_circuit <- brm(
 
 summary(fit_weather_circuit)
 write_rds(fit_weather_circuit, "dat/fit_weather_circuit.rds")
+
+# basic model, but with driver's nested in year (i.e driver form)
+fit_driver_form <- brm(
+  formula = prop_trans ~ 0 + (1 | driver) + (1 | driver:year) + (1 | constructor) + (1 | constructor:year),
+  family  = Beta(),
+  data    = f1_dat_finished,
+  backend = "cmdstanr",
+  cores   = 4,
+  threads = 3
+)
+
+summary(fit_driver_form)
+write_rds(fit_driver_form, "dat/fit_driver_form.rds")
+
+# weather model, but with driver's nested in year (i.e driver form)
+fit_driver_form_weather <- brm(
+  formula = prop_trans ~ 0 + (1 + weather_type | driver) + (1 | driver:year) + (1 | constructor) + (1 | constructor:year),
+  family  = Beta(),
+  data    = f1_dat_finished,
+  backend = "cmdstanr",
+  cores   = 4,
+  threads = 3
+)
+
+summary(fit_driver_form_weather)
+write_rds(fit_driver_form_weather, "dat/fit_driver_form_weather.rds")
