9b056e743de8c688f87d8ffc6f7817040d39e8dd
Erik-Jan van Kesteren <erikjanvankesteren@pm.me>
2022-12-12 17:08:37 +0100

    First version of pp check for ROL model

--- 05_check.R
+++ 05_check.R
@@ -3,16 +3,16 @@
 # Contents: MCMC validation, posterior predictive checks
 library(tidyverse)
 library(bayesplot)
+library(posterior)
 library(firatheme)
 
-f1_dat <- read_rds("dat/f1_dat_finished.rds")
-fit <- read_rds("fit/fit_basic.rds")
+f1_dat <- read_rds("dat/f1_dat.rds") %>% filter(finished)
+fit <- read_rds("fit/circuit.rds")
 
 # MCMC mixing ----
-mcmc_plot(fit, type = "trace") +
-  facet_wrap(~parameter, nrow = 6, scales = "free") +
+mcmc_trace(fit$draws(c("tau_driver", "tau_driver_season", "tau_team", "tau_team_season", "tau_circuit"))) +
   theme_fira() +
-  scale_colour_fira()
+  scale_colour_fira(guide = "none")
 ggsave("img/chains.png", width = 7, height = 10, bg = "white")
 
 # Rhat ----
@@ -20,62 +20,84 @@ rhats <- rhat(fit)
 any(rhats[!is.nan(rhats)] > 1.01)
 
 # 2019 posterior predictive check ----
+rgumbel <- function(n, theta) {
+  sapply(theta, function(th) th-log(-log(runif(n))))
+}
+
 # create drivers & constructors in 2019
+ordered_levels <-
+  f1_dat %>%
+  filter(year == 2019) %>%
+  select(driver, constructor, year, position) %>%
+  group_by(driver) %>%
+  summarise(pos = mean(position)) %>%
+  arrange(pos) %>%
+  pull(driver) %>%
+  as.character()
+
 pred_tab <-
   f1_dat %>%
   filter(year == 2019) %>%
   select(driver, constructor, year) %>%
-  distinct() %>%
-  mutate(weather_type = "dry", circuit_type = "permanent")
+  distinct()
 
-# predict proportion of outperformed drivers
-pp_tab <- posterior_predict(fit, pred_tab)
+idx_driver <- pred_tab$driver |> as.integer()
+idx_team <- pred_tab$constructor |> as.integer()
+
+driver_skill <- fit$draws("theta_driver")
+driver_form <- subset_draws(fit$draws("theta_driver_season"), "theta_driver_season\\[\\d+\\,6\\]", regex = TRUE)
+circuit_effect <- fit$draws("circuit_effect")
+team_skill <- fit$draws("theta_team")
+team_form <- subset_draws(fit$draws("theta_team_season"), "theta_team_season\\[\\d+\\,6\\]", regex = TRUE)
 
 
-## Proportion plot ----
-# yrep
-pred_tab_long <-
-  pred_tab %>%
-  bind_cols(t(pp_tab) %>% as_tibble(.name_repair = "minimal") %>% set_names(1:10000)) %>%
+latent_skill <-
+  as_draws_df(
+    driver_skill[,,idx_driver] +
+      driver_form[,,idx_driver] +
+      circuit_effect[,,idx_driver] +
+      team_skill[,,idx_team] +
+      team_form[,,idx_team]
+  ) |>
   pivot_longer(
-    cols      = c(-driver, -constructor, -year, -weather_type, -circuit_type),
-    names_to  = "sample",
-    values_to = "prop_trans"
-  ) %>%
+    starts_with("theta_driver"),
+    names_to = c("driver_id"),
+    names_pattern = "theta_driver\\[(\\d+)]",
+    names_transform = as.integer
+  ) |>
+  mutate(driver_name = levels(f1_dat |> pull(driver))[driver_id]) |>
+  select(.draw, driver_name, value) |>
+  mutate(performance = rgumbel(1, value))
+
+
+# TODO: make pp check on a per-race basis, not all races have 20 competitors!
+yrep <-
+  latent_skill |>
+  group_by(.draw) |>
+  summarize(driver_name = driver_name, .draw = .draw, position = rank(-performance)) |>
+  ungroup() |>
   mutate(origin = "simulated")
 
-# y
-true_tab_long <-
+y <-
   f1_dat %>%
   filter(year == 2019) %>%
-  select(driver, constructor, year, prop_trans) %>%
+  select(driver_name = driver, position) %>%
   mutate(origin = "observed")
 
-ordered_levels <-
-  true_tab_long %>%
-  group_by(driver) %>%
-  summarise(prop = mean(prop_trans)) %>%
-  arrange(-prop) %>%
-  pull(driver) %>%
-  as.character()
-
-
-bind_rows(pred_tab_long, true_tab_long) %>%
-  ggplot(aes(x = prop_trans, fill = origin)) +
-  geom_density(alpha = 0.8, bw = .07) +
-  facet_wrap(~factor(driver, levels = ordered_levels), scales = "free") +
-  xlim(0, 1) +
-  theme_fira() +
-  scale_fill_fira() +
-  theme(legend.position = "top") +
+bind_rows(y, yrep) |>
+  ggplot(aes(x = factor(position), fill = origin, group = origin)) +
+  geom_bar(aes(y = after_stat(prop)), position = position_dodge(preserve = "single")) +
+  facet_wrap(~factor(driver_name, levels = ordered_levels)) +
+  theme_linedraw() +
+  theme(legend.position = "top", axis.text.x = element_text(angle = 70, vjust = 1)) +
   labs(
     title = "2019 season posterior predictive check",
-    x = "Proportion of outperformed drivers",
+    x = "Position",
     y = "",
     fill = ""
   )
 
-ggsave("img/pp_check_prop_2019.png", width = 15, height = 12, bg = "white")
+ggsave("img/pp_check_rank_2019.png", width = 15, height = 12, bg = "white")
 
 
 
